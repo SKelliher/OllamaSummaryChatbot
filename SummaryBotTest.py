625509e90f6f795a3d7a64863f8dace4d0b5ddc4
St John Kelliher <stjmk63@gmail.com>
2024-04-29 13:39:10 -0400

    Add SummaryBotTest

---
+++ SummaryBotTest.py
@@ -0,0 +1,13 @@
+
+# from SummaryBot import SummaryBot
+
+# # Example usage:
+# fileTag = 'BooksBricksandBoards_240427_uDPzX9SzppQ'
+# summaryCount = 10
+# model = 'wizardlm2'
+# systemPrompt = 'You are reading a YouTube transcript and will provide a summary'
+# queryPrompt = 'Summarize just the text sequentially in ' + str(summaryCount) + ' detailed and numbered points. Include a short caption to begin each point '
+# outputHeader = 'VideoID-uDPzX9SzppQ'
+
+# bot = SummaryBot(fileTag, model, 1, summaryCount, systemPrompt, queryPrompt,outputHeader)
+# bot.run()
